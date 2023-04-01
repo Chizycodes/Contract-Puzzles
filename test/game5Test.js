2200af31ea11a18b9eb864d3f349b324de33fdc9
Chizycodes <okwudirejoy@gmail.com>
2023-04-01 16:31:24 +0100

    pass 5

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,5 +1,6 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
@@ -12,8 +13,26 @@ describe('Game5', function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    let threshold = 0x00ffffffffffffffffffffffffffffffffffffff;
+    let wallet;
+    let address;
+    let valid = false;
 
-    await game.win();
+    while(!valid) {
+      wallet = ethers.Wallet.createRandom();
+      address = await wallet.getAddress();
+      valid = address < threshold;
+    }
+
+    wallet = wallet.connect(ethers.provider);
+    const signer = ethers.provider.getSigner(0);
+
+    await signer.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther('1.0')
+    })
+
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
